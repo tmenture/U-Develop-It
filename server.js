bcfdf5f5a08990e21cce13a4530be88901b79a14
tmenture <thomasoxemail@gmail.com>
2022-05-18 15:05:24 -0400

    added first query

--- server.js
+++ server.js
@@ -2,10 +2,26 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Expres middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// Connecting to Database
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'MarLey2015!!',
+        database: 'election'
+    },
+    console.log('Conneted to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other requests not found
 app.use((req, res) => {
