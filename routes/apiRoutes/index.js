aa47b060fd82399701f873de784d5c85592e8696
tmenture <thomasoxemail@gmail.com>
2022-05-20 21:13:17 -0400

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(require('./voterRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./candidateRoutes'));
 
